fc4558ecfb04056ec68b492c2d5780d3064509d0
Sunghyun Kim <wkddls423@gmail.com>
2023-06-25 18:19:19 +0900

    학생 데이터 표현 클래스 추가 - 하위정보 : 이름 / 출생년도

---
+++ app/src/main/java/com/example/listviewpractice/datas/Student.java
@@ -0,0 +1,11 @@
+package com.example.listviewpractice.datas;
+
+public class Student {
+
+    private String name;
+    private int birthYear;
+
+    public Student(String name, int birthYear){
+
+    }
+}
