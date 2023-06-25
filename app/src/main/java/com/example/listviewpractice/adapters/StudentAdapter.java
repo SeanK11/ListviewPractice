ba39de1b4ad5f78ef060a2d0b98d21f6bf4f4836
Sunghyun Kim <wkddls423@gmail.com>
2023-06-25 19:43:42 +0900

    Adapter 뼈대 작업 마무리

---
+++ app/src/main/java/com/example/listviewpractice/adapters/StudentAdapter.java
@@ -0,0 +1,41 @@
+package com.example.listviewpractice.adapters;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import com.example.listviewpractice.R;
+import com.example.listviewpractice.datas.Student;
+
+import java.util.ArrayList;
+
+public class StudentAdapter extends ArrayAdapter<Student> {
+    private Context mContext;
+    private int resId;
+    private ArrayList<Student> mList;
+    private LayoutInflater inf;
+
+    public StudentAdapter(Context context, int resId, ArrayList<Student> student){
+        super(context, resId, student);
+        this.mContext = context;
+        this.resId = resId;
+        this.mList = student;
+        this.inf = LayoutInflater.from(mContext);
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        View tempRow = convertView;
+        if (tempRow == null) {
+            tempRow = inf.inflate(R.layout.student_list_item, null);
+        }
+
+        View row = tempRow != null ? tempRow :;
+        return row;
+    }
+}
